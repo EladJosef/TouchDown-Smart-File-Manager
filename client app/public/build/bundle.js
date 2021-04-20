781b4517f8b68b2855f12cf09de92f2e92074f08
Elad Josef <eladyosef2003@gmail.com>
2021-04-20 20:28:25 +0300

    Add login screen to main page

--- client app/public/build/bundle.js
+++ client app/public/build/bundle.js
@@ -40,6 +40,16 @@ var app = (function () {
     function element(name) {
         return document.createElement(name);
     }
+    function text(data) {
+        return document.createTextNode(data);
+    }
+    function space() {
+        return text(' ');
+    }
+    function listen(node, event, handler, options) {
+        node.addEventListener(event, handler, options);
+        return () => node.removeEventListener(event, handler, options);
+    }
     function attr(node, attribute, value) {
         if (value == null)
             node.removeAttribute(attribute);
@@ -267,6 +277,19 @@ var app = (function () {
         dispatch_dev('SvelteDOMRemove', { node });
         detach(node);
     }
+    function listen_dev(node, event, handler, options, has_prevent_default, has_stop_propagation) {
+        const modifiers = options === true ? ['capture'] : options ? Array.from(Object.keys(options)) : [];
+        if (has_prevent_default)
+            modifiers.push('preventDefault');
+        if (has_stop_propagation)
+            modifiers.push('stopPropagation');
+        dispatch_dev('SvelteDOMAddEventListener', { node, event, handler, modifiers });
+        const dispose = listen(node, event, handler, options);
+        return () => {
+            dispatch_dev('SvelteDOMRemoveEventListener', { node, event, handler, modifiers });
+            dispose();
+        };
+    }
     function attr_dev(node, attribute, value) {
         attr(node, attribute, value);
         if (value == null)
@@ -307,28 +330,269 @@ var app = (function () {
 
     function create_fragment(ctx) {
     	let main;
-    	let h1;
+    	let div0;
+    	let img;
+    	let img_src_value;
+    	let t0;
+    	let div7;
+    	let div1;
+    	let form0;
+    	let h10;
+    	let t2;
+    	let span0;
+    	let t4;
+    	let input0;
+    	let t5;
+    	let input1;
+    	let t6;
+    	let input2;
+    	let t7;
+    	let button0;
+    	let t9;
+    	let div2;
+    	let form1;
+    	let h11;
+    	let t11;
+    	let span1;
+    	let t13;
+    	let input3;
+    	let t14;
+    	let input4;
+    	let t15;
+    	let button1;
+    	let t17;
+    	let div6;
+    	let div5;
+    	let div3;
+    	let h12;
+    	let t19;
+    	let p0;
+    	let t21;
+    	let button2;
+    	let t23;
+    	let div4;
+    	let h13;
+    	let t25;
+    	let p1;
+    	let t27;
+    	let button3;
+    	let div7_class_value;
+    	let mounted;
+    	let dispose;
 
     	const block = {
     		c: function create() {
     			main = element("main");
-    			h1 = element("h1");
-    			attr_dev(h1, "class", "");
-    			add_location(h1, file, 5, 1, 46);
-    			add_location(main, file, 4, 0, 38);
+    			div0 = element("div");
+    			img = element("img");
+    			t0 = space();
+    			div7 = element("div");
+    			div1 = element("div");
+    			form0 = element("form");
+    			h10 = element("h1");
+    			h10.textContent = "Create Account";
+    			t2 = space();
+    			span0 = element("span");
+    			span0.textContent = "or use your email for registration";
+    			t4 = space();
+    			input0 = element("input");
+    			t5 = space();
+    			input1 = element("input");
+    			t6 = space();
+    			input2 = element("input");
+    			t7 = space();
+    			button0 = element("button");
+    			button0.textContent = "Sign Up";
+    			t9 = space();
+    			div2 = element("div");
+    			form1 = element("form");
+    			h11 = element("h1");
+    			h11.textContent = "Sign in";
+    			t11 = space();
+    			span1 = element("span");
+    			span1.textContent = "or use your account";
+    			t13 = space();
+    			input3 = element("input");
+    			t14 = space();
+    			input4 = element("input");
+    			t15 = space();
+    			button1 = element("button");
+    			button1.textContent = "Sign In";
+    			t17 = space();
+    			div6 = element("div");
+    			div5 = element("div");
+    			div3 = element("div");
+    			h12 = element("h1");
+    			h12.textContent = "Welcome Back!";
+    			t19 = space();
+    			p0 = element("p");
+    			p0.textContent = "To keep connected with us please login with your\n\t\t\t\t\t\tpersonal info";
+    			t21 = space();
+    			button2 = element("button");
+    			button2.textContent = "Sign In";
+    			t23 = space();
+    			div4 = element("div");
+    			h13 = element("h1");
+    			h13.textContent = "Hello, Friend!";
+    			t25 = space();
+    			p1 = element("p");
+    			p1.textContent = "Enter your personal details and start journey with us";
+    			t27 = space();
+    			button3 = element("button");
+    			button3.textContent = "Sign Up";
+    			if (img.src !== (img_src_value = "")) attr_dev(img, "src", img_src_value);
+    			attr_dev(img, "alt", "");
+    			attr_dev(img, "class", "logo-img svelte-16vn696");
+    			add_location(img, file, 7, 2, 90);
+    			attr_dev(div0, "class", "logo svelte-16vn696");
+    			add_location(div0, file, 6, 1, 69);
+    			attr_dev(h10, "class", "svelte-16vn696");
+    			add_location(h10, file, 15, 4, 305);
+    			attr_dev(span0, "class", "svelte-16vn696");
+    			add_location(span0, file, 16, 4, 333);
+    			attr_dev(input0, "type", "text");
+    			attr_dev(input0, "placeholder", "Name");
+    			attr_dev(input0, "class", "svelte-16vn696");
+    			add_location(input0, file, 17, 4, 385);
+    			attr_dev(input1, "type", "email");
+    			attr_dev(input1, "placeholder", "Email");
+    			attr_dev(input1, "class", "svelte-16vn696");
+    			add_location(input1, file, 18, 4, 430);
+    			attr_dev(input2, "type", "password");
+    			attr_dev(input2, "placeholder", "Password");
+    			attr_dev(input2, "class", "svelte-16vn696");
+    			add_location(input2, file, 19, 4, 477);
+    			attr_dev(button0, "class", "svelte-16vn696");
+    			add_location(button0, file, 20, 4, 530);
+    			attr_dev(form0, "action", "#");
+    			attr_dev(form0, "class", "svelte-16vn696");
+    			add_location(form0, file, 14, 3, 283);
+    			attr_dev(div1, "class", "form-container sign-up-container svelte-16vn696");
+    			add_location(div1, file, 13, 2, 233);
+    			attr_dev(h11, "class", "svelte-16vn696");
+    			add_location(h11, file, 25, 4, 649);
+    			attr_dev(span1, "class", "svelte-16vn696");
+    			add_location(span1, file, 26, 4, 670);
+    			attr_dev(input3, "type", "email");
+    			attr_dev(input3, "placeholder", "Email");
+    			attr_dev(input3, "class", "svelte-16vn696");
+    			add_location(input3, file, 27, 4, 707);
+    			attr_dev(input4, "type", "password");
+    			attr_dev(input4, "placeholder", "Password");
+    			attr_dev(input4, "class", "svelte-16vn696");
+    			add_location(input4, file, 28, 4, 754);
+    			attr_dev(button1, "class", "svelte-16vn696");
+    			add_location(button1, file, 29, 4, 807);
+    			attr_dev(form1, "action", "#");
+    			attr_dev(form1, "class", "svelte-16vn696");
+    			add_location(form1, file, 24, 3, 627);
+    			attr_dev(div2, "class", "form-container sign-in-container svelte-16vn696");
+    			add_location(div2, file, 23, 2, 577);
+    			attr_dev(h12, "class", "svelte-16vn696");
+    			add_location(h12, file, 35, 5, 961);
+    			attr_dev(p0, "class", "svelte-16vn696");
+    			add_location(p0, file, 36, 5, 989);
+    			attr_dev(button2, "class", "ghost svelte-16vn696");
+    			attr_dev(button2, "id", "signIn");
+    			add_location(button2, file, 40, 5, 1083);
+    			attr_dev(div3, "class", "overlay-panel overlay-left svelte-16vn696");
+    			add_location(div3, file, 34, 4, 915);
+    			attr_dev(h13, "class", "svelte-16vn696");
+    			add_location(h13, file, 47, 5, 1255);
+    			attr_dev(p1, "class", "svelte-16vn696");
+    			add_location(p1, file, 48, 5, 1284);
+    			attr_dev(button3, "class", "ghost svelte-16vn696");
+    			attr_dev(button3, "id", "signUp");
+    			add_location(button3, file, 49, 5, 1350);
+    			attr_dev(div4, "class", "overlay-panel overlay-right svelte-16vn696");
+    			add_location(div4, file, 46, 4, 1208);
+    			attr_dev(div5, "class", "overlay svelte-16vn696");
+    			add_location(div5, file, 33, 3, 889);
+    			attr_dev(div6, "class", "overlay-container svelte-16vn696");
+    			add_location(div6, file, 32, 2, 854);
+
+    			attr_dev(div7, "class", div7_class_value = " " + (/*sign_up*/ ctx[0]
+    			? "container right-panel-active"
+    			: "container") + " svelte-16vn696");
+
+    			attr_dev(div7, "id", "container");
+    			add_location(div7, file, 9, 1, 138);
+    			attr_dev(main, "class", "svelte-16vn696");
+    			add_location(main, file, 5, 0, 61);
     		},
     		l: function claim(nodes) {
     			throw new Error("options.hydrate only works if the component was compiled with the `hydratable: true` option");
     		},
     		m: function mount(target, anchor) {
     			insert_dev(target, main, anchor);
-    			append_dev(main, h1);
+    			append_dev(main, div0);
+    			append_dev(div0, img);
+    			append_dev(main, t0);
+    			append_dev(main, div7);
+    			append_dev(div7, div1);
+    			append_dev(div1, form0);
+    			append_dev(form0, h10);
+    			append_dev(form0, t2);
+    			append_dev(form0, span0);
+    			append_dev(form0, t4);
+    			append_dev(form0, input0);
+    			append_dev(form0, t5);
+    			append_dev(form0, input1);
+    			append_dev(form0, t6);
+    			append_dev(form0, input2);
+    			append_dev(form0, t7);
+    			append_dev(form0, button0);
+    			append_dev(div7, t9);
+    			append_dev(div7, div2);
+    			append_dev(div2, form1);
+    			append_dev(form1, h11);
+    			append_dev(form1, t11);
+    			append_dev(form1, span1);
+    			append_dev(form1, t13);
+    			append_dev(form1, input3);
+    			append_dev(form1, t14);
+    			append_dev(form1, input4);
+    			append_dev(form1, t15);
+    			append_dev(form1, button1);
+    			append_dev(div7, t17);
+    			append_dev(div7, div6);
+    			append_dev(div6, div5);
+    			append_dev(div5, div3);
+    			append_dev(div3, h12);
+    			append_dev(div3, t19);
+    			append_dev(div3, p0);
+    			append_dev(div3, t21);
+    			append_dev(div3, button2);
+    			append_dev(div5, t23);
+    			append_dev(div5, div4);
+    			append_dev(div4, h13);
+    			append_dev(div4, t25);
+    			append_dev(div4, p1);
+    			append_dev(div4, t27);
+    			append_dev(div4, button3);
+
+    			if (!mounted) {
+    				dispose = [
+    					listen_dev(button2, "click", /*click_handler*/ ctx[2], false, false, false),
+    					listen_dev(button3, "click", /*click_handler_1*/ ctx[3], false, false, false)
+    				];
+
+    				mounted = true;
+    			}
     		},
-    		p: noop,
+    		p: function update(ctx, [dirty]) {
+    			if (dirty & /*sign_up*/ 1 && div7_class_value !== (div7_class_value = " " + (/*sign_up*/ ctx[0]
+    			? "container right-panel-active"
+    			: "container") + " svelte-16vn696")) {
+    				attr_dev(div7, "class", div7_class_value);
+    			}
+    		},
     		i: noop,
     		o: noop,
     		d: function destroy(detaching) {
     			if (detaching) detach_dev(main);
+    			mounted = false;
+    			run_all(dispose);
     		}
     	};
 
@@ -346,34 +610,39 @@ var app = (function () {
     function instance($$self, $$props, $$invalidate) {
     	let { $$slots: slots = {}, $$scope } = $$props;
     	validate_slots("App", slots, []);
-    	let { name } = $$props;
-    	const writable_props = ["name"];
+    	let { debug } = $$props;
+    	let sign_up = false;
+    	const writable_props = ["debug"];
 
     	Object.keys($$props).forEach(key => {
     		if (!~writable_props.indexOf(key) && key.slice(0, 2) !== "$$") console.warn(`<App> was created with unknown prop '${key}'`);
     	});
 
+    	const click_handler = () => $$invalidate(0, sign_up = false);
+    	const click_handler_1 = () => $$invalidate(0, sign_up = true);
+
     	$$self.$$set = $$props => {
-    		if ("name" in $$props) $$invalidate(0, name = $$props.name);
+    		if ("debug" in $$props) $$invalidate(1, debug = $$props.debug);
     	};
 
-    	$$self.$capture_state = () => ({ name });
+    	$$self.$capture_state = () => ({ debug, sign_up });
 
     	$$self.$inject_state = $$props => {
-    		if ("name" in $$props) $$invalidate(0, name = $$props.name);
+    		if ("debug" in $$props) $$invalidate(1, debug = $$props.debug);
+    		if ("sign_up" in $$props) $$invalidate(0, sign_up = $$props.sign_up);
     	};
 
     	if ($$props && "$$inject" in $$props) {
     		$$self.$inject_state($$props.$$inject);
     	}
 
-    	return [name];
+    	return [sign_up, debug, click_handler, click_handler_1];
     }
 
     class App extends SvelteComponentDev {
     	constructor(options) {
     		super(options);
-    		init(this, options, instance, create_fragment, safe_not_equal, { name: 0 });
+    		init(this, options, instance, create_fragment, safe_not_equal, { debug: 1 });
 
     		dispatch_dev("SvelteRegisterComponent", {
     			component: this,
@@ -385,16 +654,16 @@ var app = (function () {
     		const { ctx } = this.$$;
     		const props = options.props || {};
 
-    		if (/*name*/ ctx[0] === undefined && !("name" in props)) {
-    			console.warn("<App> was created without expected prop 'name'");
+    		if (/*debug*/ ctx[1] === undefined && !("debug" in props)) {
+    			console.warn("<App> was created without expected prop 'debug'");
     		}
     	}
 
-    	get name() {
+    	get debug() {
     		throw new Error("<App>: Props cannot be read directly from the component instance unless compiling with 'accessors: true' or '<svelte:options accessors/>'");
     	}
 
-    	set name(value) {
+    	set debug(value) {
     		throw new Error("<App>: Props cannot be set directly on the component instance unless compiling with 'accessors: true' or '<svelte:options accessors/>'");
     	}
     }
@@ -402,7 +671,7 @@ var app = (function () {
     const app = new App({
         target: document.body,
         props: {
-            debug: false
+            debug: true
         }
     });
 
--- client app/src/App.svelte
+++ client app/src/App.svelte
@@ -1,10 +1,247 @@
 <script>
-	export let name;
+	export let debug;
+	let sign_up = false;
 </script>
 
 <main>
-	<h1 class="" />
+	<div class="logo">
+		<img src="" alt="" class="logo-img" />
+	</div>
+	<div
+		class=" {sign_up ? 'container right-panel-active' : 'container'}"
+		id="container"
+	>
+		<div class="form-container sign-up-container">
+			<form action="#">
+				<h1>Create Account</h1>
+				<span>or use your email for registration</span>
+				<input type="text" placeholder="Name" />
+				<input type="email" placeholder="Email" />
+				<input type="password" placeholder="Password" />
+				<button>Sign Up</button>
+			</form>
+		</div>
+		<div class="form-container sign-in-container">
+			<form action="#">
+				<h1>Sign in</h1>
+				<span>or use your account</span>
+				<input type="email" placeholder="Email" />
+				<input type="password" placeholder="Password" />
+				<button>Sign In</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-left">
+					<h1>Welcome Back!</h1>
+					<p>
+						To keep connected with us please login with your
+						personal info
+					</p>
+					<button
+						on:click={() => (sign_up = false)}
+						class="ghost"
+						id="signIn">Sign In</button
+					>
+				</div>
+				<div class="overlay-panel overlay-right">
+					<h1>Hello, Friend!</h1>
+					<p>Enter your personal details and start journey with us</p>
+					<button
+						on:click={() => (sign_up = true)}
+						class="ghost"
+						id="signUp">Sign Up</button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
 
 <style>
+	@import url("https://fonts.googleapis.com/css?family=Montserrat:400,800");
+	* {
+		box-sizing: border-box;
+	}
+
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
+
+	button {
+		border-radius: 1vh;
+		border: none;
+		background-color: var(--main-color);
+		color: var(--blank-color);
+		font-size: 1.5vh;
+		font-weight: bold;
+		padding: 12px 45px;
+		letter-spacing: 1px;
+		text-transform: uppercase;
+		transition: transform 80ms ease-in;
+	}
+
+	button:active {
+		transform: scale(0.95);
+	}
+
+	button:focus {
+		outline: 0;
+	}
+
+	button.ghost {
+		background-color: transparent;
+		border-color: var(--blank-color);
+		border: 1px solid var(--blank-color);
+	}
+
+	form {
+		background-color: var(--blank-color);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		padding: 0 50px;
+		height: 100%;
+		text-align: center;
+	}
+
+	input {
+		background-color: var(--blank-color);
+		border: none;
+		padding: 12px 15px;
+		margin: 8px 0;
+		width: 100%;
+	}
+
+	.logo {
+		height: 30vh;
+		background-color: var(--main-color);
+		-webkit-app-region: drag;
+		-webkit-user-select: none;
+	}
+
+	.container {
+		user-select: none;
+		font-family: "Montserrat", sans-serif;
+	}
+
+	.container {
+		background-color: #fff;
+		position: relative;
+		overflow: hidden;
+		width: 100%;
+		height: 70vh;
+	}
+
+	.form-container {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		transition: all 0.6s ease-in-out;
+	}
+
+	.sign-in-container {
+		left: 0;
+		width: 50%;
+		z-index: 2;
+	}
+
+	.container.right-panel-active .sign-in-container {
+		transform: translateX(100%);
+	}
+
+	.sign-up-container {
+		left: 0;
+		width: 50%;
+		opacity: 0;
+		z-index: 1;
+	}
+
+	.container.right-panel-active .sign-up-container {
+		transform: translateX(100%);
+		opacity: 1;
+		z-index: 5;
+		animation: show 0.6s;
+	}
+
+	@keyframes show {
+		0%,
+		49.99% {
+			opacity: 0;
+			z-index: 1;
+		}
+
+		50%,
+		100% {
+			opacity: 1;
+			z-index: 5;
+		}
+	}
+
+	.overlay-container {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		width: 50%;
+		height: 100%;
+		overflow: hidden;
+		transition: transform 0.6s ease-in-out;
+		z-index: 100;
+	}
+
+	.container.right-panel-active .overlay-container {
+		transform: translateX(-100%);
+	}
+
+	.overlay {
+		background: var(--background-color);
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: 0 0;
+		color: var(--blank-color);
+		position: relative;
+		left: -100%;
+		height: 100%;
+		width: 200%;
+		transform: translateX(0);
+		transition: transform 0.6s ease-in-out;
+	}
+
+	.container.right-panel-active .overlay {
+		transform: translateX(50%);
+	}
+
+	.overlay-panel {
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		padding: 0 40px;
+		text-align: center;
+		top: 0;
+		height: 100%;
+		width: 50%;
+		transform: translateX(0);
+		transition: transform 0.6s ease-in-out;
+	}
+
+	.overlay-left {
+		transform: translateX(-20%);
+	}
+
+	.container.right-panel-active .overlay-left {
+		transform: translateX(0);
+	}
+
+	.overlay-right {
+		right: 0;
+		transform: translateX(0);
+	}
+
+	.container.right-panel-active .overlay-right {
+		transform: translateX(20%);
+	}
 </style>
